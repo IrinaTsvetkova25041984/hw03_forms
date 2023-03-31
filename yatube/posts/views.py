b472906c02a4872656afd475a347e9d4b9a7ad8a
Ирина Цветкова <IrinaTsvetkova25041984@yandex.ru>
2023-03-31 17:11:11 +0300

    original

---
+++ yatube/posts/views.py
@@ -0,0 +1,84 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.conf import settings
+from django.shortcuts import render, redirect, get_object_or_404
+
+
+from .models import Group, Post, User
+from .forms import PostForm
+
+
+
+def index(request):
+    posts = Post.objects.select_related('group')[:settings.LIMIT_POSTS]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, settings.LIMIT_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'posts': posts,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.select_related('group')[:settings.LIMIT_POSTS]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, settings.LIMIT_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = author.post.all()
+    posts = Post.objects.all()
+    paginator = Paginator(post_list, settings.LIMIT_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    post_number = post_list.count()
+    context = {
+        'author': author,
+        'posts': posts,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    author_posts = post.author.post.all()
+    context = {
+        'post': post,
+        'author_posts': author_posts.count(),
+    }
+    return render(request, 'posts/post_detail.html', context) 
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    post = form.save(commit=False)
+    if form.is_valid():
+        post.author = request.user
+        form.save()
+        return redirect('posts:profile', username=request.user)
+    return render(request, 'posts/post_create.html', {'form': form})
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(instance=post)
+    return render(request, 'posts/post_create.html', {'form': form, 'post': post})
