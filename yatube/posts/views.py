aebfe7fd9fb4c506f442dc5705e61952ebe5966a
Ирина Цветкова <IrinaTsvetkova25041984@yandex.ru>
2023-04-04 17:14:52 +0300

    second version after review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -29,7 +29,6 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     post_list = author.post.all()
-    post = Post.objects.all()
     page_obj = paginations(request, post_list)
     context = {
         'author': author,
